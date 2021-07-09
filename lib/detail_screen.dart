c5a0a2ba85dc42087d5c61b44da4e398fdde310d
Rizki Firdaus <69286199+rfm20@users.noreply.github.com>
2021-07-09 21:22:31 +0700

    Menambahkan daftar tempat wisata

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,22 +1,59 @@
 import 'package:flutter/material.dart';
+import './model/tourism_place.dart';
+
+var informationTextStyle = TextStyle(
+  fontFamily: 'Oxygen',
+  // color: Colors.white,
+);
 
 class DetailScreen extends StatelessWidget {
-  var informationTextStyle = TextStyle(fontFamily: 'Oxygen');
+  final TourismPlace place;
+
+  DetailScreen({required this.place});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.black,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Image.asset('images/farm-house.jpg'),
+            Stack(
+              children: <Widget>[
+                Image.asset(place.imageAsset),
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.grey.withOpacity(0.5),
+                          child: IconButton(
+                            icon: Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                        FavoriteButton(),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
             Container(
               margin: EdgeInsets.only(top: 16.0),
               child: Text(
-                'Farm House Lembang',
+                place.name,
                 textAlign: TextAlign.center,
                 style: TextStyle(
+                  // color: Colors.white,
                   fontSize: 30.0,
                   fontFamily: 'Staatliches',
                 ),
@@ -29,30 +66,39 @@ class DetailScreen extends StatelessWidget {
                 children: <Widget>[
                   Column(
                     children: <Widget>[
-                      Icon(Icons.calendar_today),
+                      Icon(
+                        Icons.calendar_today,
+                        // color: Colors.white,
+                      ),
                       SizedBox(height: 8.0),
                       Text(
-                        'Open Everyday',
+                        place.openDays,
                         style: informationTextStyle,
                       ),
                     ],
                   ),
                   Column(
                     children: <Widget>[
-                      Icon(Icons.access_time),
+                      Icon(
+                        Icons.access_time,
+                        // color: Colors.white,
+                      ),
                       SizedBox(height: 8.0),
                       Text(
-                        '09:00 - 20:00',
+                        place.openTime,
                         style: informationTextStyle,
                       ),
                     ],
                   ),
                   Column(
                     children: <Widget>[
-                      Icon(Icons.monetization_on),
+                      Icon(
+                        Icons.monetization_on,
+                        // color: Colors.white,
+                      ),
                       SizedBox(height: 8.0),
                       Text(
-                        'Rp. 25.000',
+                        place.ticketPrice,
                         style: informationTextStyle,
                       ),
                     ],
@@ -63,49 +109,57 @@ class DetailScreen extends StatelessWidget {
             Container(
               padding: EdgeInsets.all(16.0),
               child: Text(
-                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                place.description,
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16.0),
+                style: TextStyle(
+                  // color: Colors.white,
+                  fontSize: 16.0,
+                  fontFamily: 'Oxygen',
+                ),
               ),
             ),
             Container(
               height: 150,
               child: ListView(
                 scrollDirection: Axis.horizontal,
-                children: <Widget>[
-                  Padding(
+                children: place.imageUrls.map((url) {
+                  return Padding(
                     padding: const EdgeInsets.all(4.0),
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(10),
-                      child: Image.network(
-                        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg',
-                      ),
+                      child: Image.network(url),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.network(
-                        'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg',
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.network(
-                        'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg',
-                      ),
-                    ),
-                  ),
-                ],
+                  );
+                }).toList(),
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
+  @override
+  _FavoriteButtonState createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        isFavorite ? Icons.favorite : Icons.favorite_border,
+        color: Colors.red,
+      ),
+      onPressed: () {
+        setState(() {
+          isFavorite = !isFavorite;
+        });
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wisata_bandung/detail_screen.dart';
+import 'main_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,9 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Wisata Bandung',
       theme: ThemeData(),
-      home: DetailScreen(),
+      home: MainScreen(),
     );
   }
 }
